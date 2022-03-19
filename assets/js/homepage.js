349058be958df9d5aad07b300b88d00b2b29e10d
Brainybrian316 <brainybrian316@gmail.com>
2022-03-19 19:17:05 -0400

    setting up the ability to display repository data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,8 +5,33 @@ var getUserRepos = function (user) {
     // make a request to the url
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
-getUserRepos("brainybrian316");
+
+// /variables to reference and store the form element
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// function to execute upon form submission for teh browser event
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+var displayRepos = function (repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
