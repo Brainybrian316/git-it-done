fd927e1000930d97f914665d1004ff914cc01d59
Brainybrian316 <brainybrian316@gmail.com>
2022-03-20 00:13:15 -0400

    fixed loading issues with html elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -29,24 +29,24 @@ var displayIssues = function (issues) {
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
         issueContainerEl.appendChild(issueEl);
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        // append to container
+        issueEl.appendChild(typeEl);
     }
-    // create span to hold issue title
-    var titleEl = document.createElement("span");
-    titleEl.textContent = issues[i].title;
-
-    // append to container
-    issueEl.appendChild(titleEl);
-
-    // create a type element
-    var typeEl = document.createElement("span");
-
-    // check if issue is an actual issue or a pull request
-    if (issues[i].pull_request) {
-        typeEl.textContent = "(Pull request)";
-    } else {
-        typeEl.textContent = "(Issue)";
-    }
-    // append to container
-    issueEl.appendChild(typeEl);
 }
 getRepoIssues("facebook/react");
